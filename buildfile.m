4bd7101bd7fe63d0b1b089f26a48474e966ff5ad
sameagen <sameagen@vdi-ah2ddp-153>
2025-08-12 15:37:16 -0400

    Update build file

--- buildfile.m
+++ buildfile.m
@@ -11,5 +11,27 @@ plan.DefaultTasks = ["check" "test"];
 end
 
 function standaloneTask(~)
-    deploymentScript();
+projectRoot = "/home/sameagen/Deployment-MDRs";
+
+% Create target build options object, set build properties and build.
+buildOpts = compiler.build.StandaloneApplicationOptions(fullfile(projectRoot, "src", "hello.m"));
+buildOpts.AutoDetectDataFiles = true;
+buildOpts.OutputDir = fullfile(projectRoot, "derived", "build");
+buildOpts.ObfuscateArchive = false;
+buildOpts.Verbose = true;
+buildOpts.EmbedArchive = true;
+buildOpts.ExecutableName = "hello";
+buildOpts.ExecutableVersion = "1.0.0";
+buildOpts.TreatInputsAsNumeric = false;
+buildResult = compiler.build.standaloneApplication(buildOpts);
+
+
+% Create package options object, set package properties and package.
+packageOpts = compiler.package.InstallerOptions(buildResult);
+packageOpts.ApplicationName = "Hello App";
+packageOpts.AuthorName = "Sam Eagen";
+packageOpts.OutputDir = fullfile(projectRoot, "derived", "package");
+packageOpts.Summary = "App for greeting";
+packageOpts.Verbose = true;
+compiler.package.installer(buildResult, "Options", packageOpts);
 end
